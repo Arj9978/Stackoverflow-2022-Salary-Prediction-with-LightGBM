7df9c3d09b332696bc8de5bcd00a2ba05316d68b
Arj9978 <110104436+Arj9978@users.noreply.github.com>
2023-07-22 20:27:51 +0330

    Update Stackoverflow-2022-Salary-Prediction-with-LightGBM-App.py

--- Stackoverflow-2022-Salary-Prediction-with-LightGBM-App.py
+++ Stackoverflow-2022-Salary-Prediction-with-LightGBM-App.py
@@ -3,10 +3,7 @@ import numpy as np
 import pandas as pd
 import joblib
 import lightgbm
-
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler,LabelEncoder, OneHotEncoder
+from utils import PrepProcesor, columns
 
 model = joblib.load('finalized_model.joblib')
 st.title('Salary Prediction in 2022')
@@ -59,22 +56,32 @@ expericence = st.slider("Years of Experience", 0, 50, 3)
 columns = ['Country', 'EdLevel', 'YearsCodePro']
 
 ok = st.button("Calculate Salary")
-if ok:
-    X_new = np.array([country,education,expericence])
-    X_new_df = pd.DataFrame([X_new], columns = columns)
+if ok: 
+    # Create a DataFrame with the row data and columns matching the training data
+    expericence_input = pd.DataFrame(expericence, columns = ['YearsCodePro'])
 
-    categorical_pipeline = Pipeline([('OneHot', OneHotEncoder(handle_unknown='ignore'))])
+    # Perform one-hot encoding for the countries and education input
+    country_dummy = pd.get_dummies([country], columns=['Country'], prefix='', prefix_sep='')
+    education_dummy = pd.get_dummies([education], columns=['EdLevel'], prefix='', prefix_sep='')
 
-    numeric_pipeline = Pipeline([('Scaler', StandardScaler())])  
-                                 
-    transformer = ColumnTransformer([('Num', numeric_pipeline, ['YearsCodePro']), 
-                                 ('Category', categorical_pipeline, ['Country','EdLevel'])])
+    # Align the countries and education columns with the training data columns
+    country_dummy = country_dummy.reindex(columns=Country, fill_value=0)
+    education_dummy = education_dummy.reindex(columns=EdLevel, fill_value=0)
+
+    # Concatenate the address columns with the input data
+    X = pd.concat([country_dummy, education_dummy, expericence_input], axis=1)
+    st.write(X)
+
+    X = np.array(X)
+    st.write(X)
+
+
     
-    st.write(X_new_df.shape)
-    X_new_df = transformer.fit_transform(X_new_df)
-    st.write(X_new_df.shape)
+    
+    # st.write(X_new_df.shape)
+    # X_new_df = transformer.fit_transform(X_new_df)
+    # st.write(X_new_df.shape)
     
     # salary = model.predict(X_new_df)
     
-    st.subheader(f"The estimated salary is ${salary[0]:.2f}")\
-
+    st.subheader(f"The estimated salary is ${salary[0]:.2f}")
